632cba5afcd3662762553dd586ed6ee840b57622
valent1618 <valentinlemaire1618@gmail.com>
2022-09-29 02:22:03 +0200

    Fix bug prerendering document doesn't exist

--- src/pages/list/eventRemove.ts
+++ src/pages/list/eventRemove.ts
@@ -5,22 +5,24 @@ import { openModal } from '../../utils/handleModal';
 import { EmployeeToFind } from '../../utils/interfaces';
 
 function eventRemove(setToRemove: Dispatch<SetStateAction<EmployeeToFind>>) {
-  const tbody = document.getElementsByTagName('tbody')[0];
-  const trs = tbody.getElementsByTagName('tr');
+  if (typeof window !== 'undefined') {
+    const tbody = document.getElementsByTagName('tbody')[0];
+    const trs = tbody.getElementsByTagName('tr');
 
-  for (let i = 0; i < trs.length; i++) {
-    const tr = trs[i];
-    if (tr.getAttribute('data-event') !== 'true') {
-      tr.setAttribute('data-event', 'true');
-      tr.addEventListener('click', () => {
-        const tds = tr.getElementsByTagName('td');
-        setToRemove({
-          firstName: tds[0].textContent || '',
-          lastName: tds[1].textContent || '',
-          birthDate: tds[4].textContent || '',
+    for (let i = 0; i < trs.length; i++) {
+      const tr = trs[i];
+      if (tr.getAttribute('data-event') !== 'true') {
+        tr.setAttribute('data-event', 'true');
+        tr.addEventListener('click', () => {
+          const tds = tr.getElementsByTagName('td');
+          setToRemove({
+            firstName: tds[0].textContent || '',
+            lastName: tds[1].textContent || '',
+            birthDate: tds[4].textContent || '',
+          });
+          openModal();
         });
-        openModal();
-      });
+      }
     }
   }
 }
